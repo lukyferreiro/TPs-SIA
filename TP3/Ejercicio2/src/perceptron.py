4c9f9ef9b747ae3b4907c3cc3e19575f43dc731f
lukyferreiro <lferreiro@itba.edu.ar>
2023-04-26 14:07:44 -0300

    Add normalize and denormalize functions according to image of activate function

--- TP3/Ejercicio2/src/perceptron.py
+++ TP3/Ejercicio2/src/perceptron.py
@@ -45,7 +45,8 @@ class Perceptron:
                 x = np.array(self.train_input[j])
                 h = np.dot(self.weights, x)
                 Os[j] = self.activation(h)
-                self.weights += self.calculate_delta_w(x, self.train_expected_data[j] - Os[j], h)
+                expected = self.train_expected_data[j]
+                self.weights += self.calculate_delta_w(x, expected, Os[j])
 
             if (self.__mid_square_error(Os, self.train_expected_data) < self.min_error):
                 finished = True
@@ -67,31 +68,30 @@ class Perceptron:
     def __activate_non_linear_tanh(self, h):
         return math.tanh(self.beta * h)
     def __activate_non_linear_log(self, h):
-        return 1 / (1 + math.pow(math.e, -2*self.beta*h))
+        return 1 / (1 + math.pow(math.e, -2 * self.beta * h))
     
     # Funcion de Δw 
-    def calculate_delta_w(self, x, error, h):
-        theta = self.__calculate_theta_diff(h)
-        return self.learning_rate * error * theta * x
+    def calculate_delta_w(self, x, expected, O):
+        error = self.__normalize_image(expected) - O
+        theta_diff = self.__calculate_theta_diff(O)
+        return self.learning_rate * error * theta_diff * x
 
     # Funcion que calcula θ' segun el tipo de perceptron
-    def __calculate_theta_diff(self, h):
+    def __calculate_theta_diff(self, O):
         switcher = {
             "LINEAR": self.__calculate_theta_diff_linear(),
-            "NON_LINEAR_TANH": self.__calculate_theta_diff_non_linear_tanh(h),
-            "NON_LINEAR_LOG": self.__calculate_theta_diff_non_linear_log(h),
+            "NON_LINEAR_TANH": self.__calculate_theta_diff_non_linear_tanh(O),
+            "NON_LINEAR_LOG": self.__calculate_theta_diff_non_linear_log(O),
         }
 
         return switcher.get(self.perceptron_type, "Tipo de perceptron invalido")
 
     def __calculate_theta_diff_linear(self):
         return 1
-    def __calculate_theta_diff_non_linear_tanh(self, h):
-        theta = self.activation(h)
-        return self.beta * (1 - theta**2)
-    def __calculate_theta_diff_non_linear_log(self, h):
-        theta = self.activation(h)
-        return 2 * self.beta * theta * (1 - theta)
+    def __calculate_theta_diff_non_linear_tanh(self, O):
+        return self.beta * (1 - O**2)
+    def __calculate_theta_diff_non_linear_log(self, O):
+        return 2 * self.beta * O * (1 - O)
 
     # Funcion de prediccion
     def predict(self, x):
@@ -100,6 +100,43 @@ class Perceptron:
     # Función de error MSE para finalizar entrenamiento
     def __mid_square_error(self, Os, expected):
         return np.sum((expected - Os) ** 2) / len(expected)
+
+    # Funcion que normaliza los resultados segun la imagen de la funcion de activacion
+    def __normalize_image(self, values):
+        switcher = {
+            "LINEAR": self.__normalize_linear_image(values),
+            "NON_LINEAR_TANH": self.__normalize_non_linear_tanh_image(values),
+            "NON_LINEAR_LOG": self.__normalize_non_linear_log_image(values),
+        }
+
+        return switcher.get(self.perceptron_type, "Tipo de perceptron invalido")
+
+    def __normalize_linear_image(self, values):
+        return values
+    # Normalizacion para [a,b] es: X'=((X-Xmin)/(Xmax-Xmin))(b-a)+a
+    def __normalize_non_linear_tanh_image(self, values):
+        # Image = (-1,1)
+        return (2 * (values - self.min) / (self.max - self.min)) - 1
+    def __normalize_non_linear_log_image(self, values):
+        # Image = (-1,1)
+        return (values - self.min) / (self.max - self.min)
+    
+    # Funcion que desnormaliza los resultados segun la imagen de la funcion de activacion
+    def __denormalize_image(self, values):
+        switcher = {
+            "LINEAR": self.__denormalize_linear_image(values),
+            "NON_LINEAR_TANH": self.__denormalize_non_linear_tanh_image(values),
+            "NON_LINEAR_LOG": self.__denormalize_non_linear_log_image(values),
+        }
+
+        return switcher.get(self.perceptron_type, "Tipo de perceptron invalido")
+
+    def __denormalize_linear_image(self, values):
+        return values
+    def __denormalize_non_linear_tanh_image(self, values):
+        return ((values + 1) * (self.max - self.min) * 0.5) + self.min
+    def __denormalize_non_linear_log_image(self, values):
+        return values * (self.max - self.min) + self.min
 
     def __str__(self) -> str:
         return f"Perceptron {self.perceptron_type}: {self.weights}" 
