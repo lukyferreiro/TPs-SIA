462f80efb11c012c4416ffcb91bfa8afcfee1b9c
lukyferreiro <lferreiro@itba.edu.ar>
2023-04-30 17:28:48 -0300

    Finish graphs of ej1

--- TP3/Ejercicio1/src/perceptron.py
+++ TP3/Ejercicio1/src/perceptron.py
@@ -2,44 +2,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Perceptron:
-    def __init__(self, num_inputs, lr, epochs, operation):
+    def __init__(self, num_inputs, learning_rate, epochs, operation):
         self.weights = np.random.default_rng().random(num_inputs)
-        self.lr = lr
+        self.learning_rate = learning_rate
         self.epochs = epochs
         self.operation = operation
+        self.errors = []
+        self.current_epoch = 0
     
     def activation(self, x):
         return 1 if x>=0 else -1
         
     def train(self, inputs, targets):
-        target_epoch = 0
-        finished = False
 
         inputs_len = len(inputs)
-
         errors = np.empty(inputs_len)
         correct = np.empty(inputs_len)
 
-        while target_epoch < self.epochs and not finished:
+        finished = False
+
+        while self.current_epoch < self.epochs and not finished:
             for j in range(inputs_len):
                 x = np.array(inputs[j])
                 O = self.activation(np.dot(self.weights, x))
                 errors[j] = targets[j] - O
                 correct[j] = 1 if errors[j] == 0 else 0
+                self.weights += self.learning_rate * errors[j] * x.astype(float)
 
-                self.weights += self.lr * errors[j] * x.astype(float)
+            self.errors.append(np.sum(np.abs(errors)))
 
-            # Si la suma absoluta de los errores en cada paso es 0, se finaliza
-            # O si la cantidad de respuestas correctas es igual a la longitud del input
-            if (np.sum(np.abs(errors)) == 0 or self.accuracy(correct, inputs_len) == 1):
+            # Se finaliza si:
+            # --la suma absoluta de los errores en cada paso es 0
+            # --la cantidad de respuestas correctas es igual a la cantidad de entradas
+            if (self.errors[self.current_epoch] == 0 or self.calculate_accuracy(correct, inputs_len) == 1):
                 finished = True
 
-            target_epoch += 1
+            self.current_epoch += 1
 
-        print(f"Finished training in {target_epoch} epochs")
+        print(f"Finished training in {self.current_epoch} epochs")
 
-    def accuracy(self, correct, inputs_len):
-        return np.sum(correct)/inputs_len
+    def calculate_accuracy(self, correct, inputs_len):
+        return np.sum(correct) / inputs_len
 
     # En la posición x0 está el bias para tener longitudes iguales y poder utilizar np.dot
     def predict(self, x):
@@ -56,24 +59,32 @@ class Perceptron:
     
     def plot(self, input_data, expected_data):
         color_val = ['blue', 'red']
-        bool_val = ['true', 'false']
+        bool_val = ['1', '-1']
 
         # asigna un color a cada punto dependiendo de la salida de la función
         colors = [color_val[0] if o == 1 else color_val[1] for o in expected_data]
 
+
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+
         # grafica los puntos
-        plt.scatter([p[1] for p in input_data], [p[2] for p in input_data], c=colors)
+        ax1.scatter([p[1] for p in input_data], [p[2] for p in input_data], c=colors)
 
         # grafica la recta
-        x = y = range(-2, 4)
-        plt.plot(x, (-self.weights[1]*x - self.weights[0])/self.weights[2], color='green')
+        x = range(-2, 4)
+        ax1.plot(x, (-self.weights[1]*x - self.weights[0])/self.weights[2], color='green')
 
         legend_elements = [plt.Line2D([0], [0], marker='o', color='w', label=name, markerfacecolor=color, markersize=10)
                    for color, name in zip(color_val, bool_val)]
         
 
-        plt.title(f'Función de clasificación de perceptron {self.operation}')
-        plt.legend(handles=legend_elements, loc='upper right')
+        ax1.set_title(f'Clasificación lineal del problema')
+        ax1.legend(handles=legend_elements, loc='upper right')
 
-        # muestra el gráfico
+        ax2.plot(range(self.current_epoch), self.errors, color='red')
+        ax2.set_title("Rendimiento a traves de las generaciones")
+        ax2.set_xlabel('Generación')
+        ax2.set_ylabel('Error')
+
+        plt.suptitle(f"Perceptron simple escalon para {self.operation} con η={self.learning_rate}")
         plt.show()
