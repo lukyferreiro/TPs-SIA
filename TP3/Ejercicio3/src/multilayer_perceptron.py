2a6b96373cfd20b42147abba68b8b8f3daebd54f
Tomas <tomalvarez@itba.edu.ar>
2023-04-29 23:36:18 -0300

    Added methods for forward and backward propagation,predictive

--- TP3/Ejercicio3/src/multilayer_perceptron.py
+++ TP3/Ejercicio3/src/multilayer_perceptron.py
@@ -1,15 +1,17 @@
 import numpy as np
+
+
 class MultilayerPerceptron:
 
     def __init__(self, input_data, expected_data, learning_rate, epochs, training_percentage, qty_hidden_layers,
-                 qty_nodes_in_hidden_layers, output_activation, hidden_activation,optimization_method):
+                 qty_nodes_in_hidden_layers, output_activation, hidden_activation, optimization_method,gradients):
         self.training_percentage = training_percentage
 
         self.input_data = input_data
         self.expected_data = expected_data
         self.learning_rate = learning_rate
         self.epochs = epochs
-
+        self.gradients = gradients
         self.output_activation = output_activation
         self.hidden_activation = hidden_activation
         self.optimization_method = optimization_method
@@ -33,16 +35,15 @@ class MultilayerPerceptron:
     def __calculate_min_and_max(self, expected_data):
         return np.min(expected_data), np.max(expected_data)
 
-
     def train(self):
         # Inicializar parámetros de la red neuronal
         parameters = self.__init_weights()
 
         # Inicializar velocidad o momentos de primer y segundo orden según el algoritmo de optimización seleccionado
         if input_handler.optimizer == 'MOMENTUM':
-            vel = __init_velocity(self)
+            vel = self.__init_velocity()
         elif input_handler.optimizer == 'ADAM':
-            M, V = __init_adam(self)
+            M, V = self.__init_adam()
 
         # Iniciar bucle de entrenamiento por épocas
         errors = []
@@ -65,10 +66,11 @@ class MultilayerPerceptron:
 
             # Actualización de parámetros según el algoritmo de optimización seleccionado
             if input_handler.optimizer == 'MOMENTUM':
-                parameters, vel = __update_params_momentum(self, gradients,vel,input_handler.momentum_alpha)
+                parameters, vel = __update_params_momentum(self, gradients, vel, input_handler.momentum_alpha)
             elif input_handler.optimizer == 'ADAM':
                 t += 1
-                parameters, M, V = __update_params_adam(self, gradients, M, V, t, input_handler.beta1, input_handler.beta2, input_handler.epsilon)
+                parameters, M, V = __update_params_adam(self, gradients, M, V, t, input_handler.beta1,
+                                                        input_handler.beta2, input_handler.epsilon)
             else:
                 parameters = __update_params_gd(self, gradients)
 
@@ -77,13 +79,55 @@ class MultilayerPerceptron:
 
         return parameters, errors
 
+    def __excitation_backward(self):
+        pass #TODO
+
+    def __excitation_forward(self):
+        pass  # TODO
+    def __excitation_activation_forward(self):
+        pass #TODO
+
+    def __excitation_activation_backward(self):
+        pass #TODO
+
+    def __model_forward(self):
+        V = self.input_data
+        caches = []
+        L = len(self.weights) // 2  # includes weights and biases
+        # activate hidden layers
+        for l in range(1, L):  # l=0 is the input
+            V_prev = V
+            b = self.weights['b' + str(l)]
+            V, cache = excitation_activation_forward(self.weights['W' + str(l)], b, self.hidden_activation)
+            caches.append(cache)
+            # activate output layer
+            O, cache = excitation_activation_forward(self.weights['W' + str(L)], self.weights['b' + str(L)],
+                                                     self.output_activation)
+        caches.append(cache)
+        return O, caches
+
+    def __model_backward(self,O, Y, caches):
+        gradients = {}
+        L = len(caches)  # number of layers
+        Y = Y.reshape(O.shape)
+        dO = O - Y
+        current_cache = caches[L - 1]
+        gradients['dV' + str(L - 1)], gradients['dW' + str(L)], gradients[
+                'db' + str(L)] = excitation_activation_backward()
+        for l in reversed(range(L - 1)):
+            current_cache = caches[l]
+                gradients['dV' + str(l)], gradients['dW' + str(l + 1)], gradients[
+                    'db' + str(l + 1)] = excitation_activation_backward(self.gradients['dV' + str(l + 1)], current_cache,
+                                                                        self.hidden_activation)
+        return
+
     def optimization_selector(self):
         switcher = {
             "MOMENTUM": self.__init_velocity(),
             "ADAM": self.__init_adam(),
             "GRADIENT": self.__update_params_gd()
         }
-        return switcher.get(self.optimization_method,"Metodo de optimizacion invalido")
+        return switcher.get(self.optimization_method, "Metodo de optimizacion invalido")
 
     def __init_adam(self):
         M = {}  # first moment
@@ -106,15 +150,15 @@ class MultilayerPerceptron:
             M_hat['dW' + str(l)] = M['dW' + str(l)] / (1 - np.power(beta1, t))  # bias-corrected 1st moment estimate
             V_hat['dW' + str(l)] = V['dW' + str(l)] / (1 - np.power(beta2, t))  # bias-corrected 2nd moment estimate
             parameters['W' + str(l)] = parameters['W' + str(l)] - self.learning_rate * M_hat['dW' + str(l)] / (
-            np.sqrt(V_hat['dW' + str(l)]) + epsilon)  # update parameters
+                    np.sqrt(V_hat['dW' + str(l)]) + epsilon)  # update parameters
             M['db' + str(l)] = beta1 * M['db' + str(l)] + (1 - beta1) * gradients[
-            'db' + str(l)]  # 1st moment estimate
+                'db' + str(l)]  # 1st moment estimate
             V['db' + str(l)] = beta2 * V['db' + str(l)] + (1 - beta2) * np.power(gradients['db' + str(l)],
-                                                                                     2)  # 2nd moment estimate
+                                                                                 2)  # 2nd moment estimate
             M_hat['db' + str(l)] = M['db' + str(l)] / (1 - np.power(beta1, t))  # bias-corrected 1st moment estimate
             V_hat['db' + str(l)] = V['db' + str(l)] / (1 - np.power(beta2, t))  # bias-corrected 2nd moment estimate
             parameters['b' + str(l)] = parameters['b' + str(l)] - self.learning_rate * M_hat['db' + str(l)] / (
-                            np.sqrt(V_hat['db' + str(l)]) + epsilon)  # update parameters
+                    np.sqrt(V_hat['db' + str(l)]) + epsilon)  # update parameters
         return parameters, M, V
 
     def __update_params_gd(self, gradients):
@@ -131,12 +175,45 @@ class MultilayerPerceptron:
             vel['db' + str(l)] = np.zeros(self.weights['b' + str(l)].shape)
         return vel
 
-# Use gradient descent with momentum to update the parameters
-    def __update_params_momentum(self, gradients,vel, alpha):
+    # Use gradient descent with momentum to update the parameters
+    def __update_params_momentum(self, gradients, vel, alpha):
         parameters = self.weights.copy()
         for l in range(1, self.qty_hidden_layers):
-            vel['dW' + str(l)] = alpha * vel['dW' + str(l)] + (1-alpha) * gradients['dW' + str(l)]
+            vel['dW' + str(l)] = alpha * vel['dW' + str(l)] + (1 - alpha) * gradients['dW' + str(l)]
             parameters['W' + str(l)] = parameters['W' + str(l)] - self.learning_rate * vel['dW' + str(l)]
-            vel['db' + str(l)] = alpha * vel['db' + str(l)] + (1-alpha) * gradients['db' + str(l)]
+            vel['db' + str(l)] = alpha * vel['db' + str(l)] + (1 - alpha) * gradients['db' + str(l)]
             parameters['b' + str(l)] = parameters['b' + str(l)] - self.learning_rate * vel['db' + str(l)]
         return parameters, vel
+
+
+    def __logistic_prediction(self):
+        O, _ = self.model_forward(self)
+        P = 1 * (O > 0.5)
+        return O, P
+
+    def __linear_prediction(self):
+        O, _ = self.model_forward(self)
+        return O, _
+
+    def __tanh_prediction(self):
+        O, _ = self.__model_forward(self)
+        P = 2 * (O > 0.5) - 1
+        return O, P
+
+    def __predict(self):
+        if self.output_activation == SIGMOID:
+            return self.__logistic_prediction(self)
+        return self.__tanh_prediction(self)
+
+    def __predict_decision_boundary(self):
+        O, _ = self.__model_forward(self)
+        P = (O > 0.5)
+        return P
+
+    def __predict_multiclass(self):
+        O, _ = self.__model_forward(self)
+        P = []
+        for output in O:
+            max_val = np.max(output)
+            P.append(1 * (output == max_val))
+        return O, P
