869adb6c69fe7ab6b9747431ce4c6cc12e475296
lukyferreiro <lferreiro@itba.edu.ar>
2023-05-09 21:58:26 -0300

    Advance with Kohonen

--- TP4/src/Kohonen.py
+++ TP4/src/Kohonen.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy
 
 class Kohonen:
 
@@ -12,11 +13,37 @@ class Kohonen:
 
     def __init_weights(self):
         self.weights = np.zeros((self.k, self.k, len(self.data[0])))
+        #Iinicializamos los pesos con muestras aleatorias de los datos de entrada
         for i in range(self.k):
             for j in range(self.k):
                 random_idx = np.random.default_rng().choice(np.arange(len(self.data)))
                 self.weights[i, j, :] = self.data[random_idx]
         print("---------------------------------------")
         print(self.weights)
+
+    def train(self):
+        current_epoch = 0
+        converged = False
+        while current_epoch < self.epochs and not converged:
+            # Seleccionar un registro de entrada Xp
+            random_idx = np.random.default_rng().choice(np.arange(len(self.data)))
+            Xp = self.data[random_idx]
+            # Encontrar la neurona ganadora que tenga el vector de pesos mas cercano a Xp
+            winner = self.__find_winner_neuron(Xp)
+            # Actualizar los pesos de las neuronas vecinas segun la regla de Kohonen
+            self.__update_weights(Xp, winner)
+            # Ver cuando converge
+            if ...:
+                converged = True
+            # Actualizar learning rate y radius ??
+            current_epoch += 1
+
+    # Encontrar la neurona ganadora que tenga el vector de pesos mas cercano a Xp
+    def __find_winner_neuron(self, Xp):
+        pass
+    
+    def __update_weights(self, Xp, winner):
+        pass
+        
 
     
